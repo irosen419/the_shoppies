ca7f9bd4eb973213292e53516dfdf26887190b37
irosen419 <irosen419@gmail.com>
2020-12-17 12:21:48 -0500

    finalized results container scroll and sizing

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,13 +1,17 @@
 function MovieCard({ movie, addOrRemove, list }) {
+    console.log(movie)
     return (
         <div className='movie-card'>
-            <h3>{movie.Title} ({movie.Year})</h3>
+            <h4>{movie.Title} ({movie.Year})</h4>
             <img src={movie.Poster} alt={movie.Title} />
             <br></br>
-            <h3>View this movie on IMDB <a href={`https://www.imdb.com/title/${movie.imdbID}/`} target="_blank" >here</a>!</h3>
-            <button onClick={() => addOrRemove(movie, list)}>{list === 'results' ? 'Nominate' : 'Remove Nomination'}</button>
+            <div className='bottom'>
+                <p>View this movie on IMDB <a href={`https://www.imdb.com/title/${movie.imdbID}/`} target="_blank" >here</a>!</p>
+                <button onClick={() => addOrRemove(movie, list)}>{list === 'results' ? 'Nominate' : 'Remove Nomination'}</button>
+            </div>
         </div>
     )
 }
 
 export default MovieCard
+
